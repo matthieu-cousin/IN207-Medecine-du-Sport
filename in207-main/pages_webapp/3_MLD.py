e2e7af24817ba033c7e307d18d1d5ab26f08cee3
Matthieu Cousin <matthieu.cousin@ensta-paris.fr>
2026-02-25 17:03:24 +0100

    Ajout des 10 requêtes en algèbre relationnelle

--- in207-main/pages_webapp/3_MLD.py
+++ in207-main/pages_webapp/3_MLD.py
@@ -61,33 +61,72 @@ Voici les opérateurs principaux que nous utiliserons :
 # Opérateurs de base
 st.subheader("Opérateurs de base")
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 with col1:
-    st.markdown("**Sélection (σ)** : Filtre les lignes selon une condition")
+    st.markdown("**Sélection (σ)** : Filtre les lignes selon une condition",text_alignment="center")
     st.latex(r"\sigma_{condition}(R)")  
 
-with col2:
-    st.markdown("**Projection (π)** : Sélectionne certaines colonnes")
+    st.markdown("**Projection (π)** : Sélectionne certaines colonnes",text_alignment="center")
     st.latex(r"\pi_{attributs}(R)")
 
-with col3:
-    st.markdown("**Jointure (⋈)** : Combine deux tables sur un attribut commun")
+with col2:
+    st.markdown("**Jointure (⋈)** : Combine deux tables sur un attribut commun",text_alignment="center")
     st.latex(r"R \bowtie S")
+
+    st.markdown("**Division (÷)** : Sélectionne les lignes qui correspondent à tous les attributs du diviseur",text_alignment="center")
+    st.latex(r"R \div S")
 
 st.markdown("---")
 
 # Exemples de requêtes en algèbre relationnelle
-st.subheader("Exemples de requêtes")
+st.header("10 Requêtes en algèbre relationnelle",text_alignment="center")
 
-st.markdown("**1. Sélectionner les étudiants de plus de 20 ans :**")
-st.latex(r"\sigma_{age > 20}(Etudiant)")
 
-st.markdown("**2. Afficher uniquement les noms et prénoms :**")
-st.latex(r"\pi_{nom, prenom}(Etudiant)")
+st.subheader("Bloc 1 : Identification et Profilage")
 
-st.markdown("**3. Noms des étudiants de plus de 20 ans :**")
-st.latex(r"\pi_{nom}(\sigma_{age > 20}(Etudiant))")
+st.markdown("**R1 : Quel(le)s sont les joueurs(euses) de basket étant en surpoids ? (bmi (= IMC) > 28)**")
+st.latex(r"R_1 = \pi_{id\_athlete} ( \sigma_{bmi > 28 \land sport = 'Basketball'} (ATHLETE) )")
+
+st.markdown("**R2 : Quels sont les sports étudiés ?**")
+st.latex(r"R_2 = \pi_{sport} (ATHLETE)")
+
+
+st.subheader("Bloc 2 : Analyse de l'Entraînement")
+
+st.markdown("**R3 : Lister la durée des séances d'entrainement de l'athlète 150.**")
+st.latex(r"R_3 = \pi_{date\_seance, duree\_minutes} ( \sigma_{id\_athlete = 150} (ENTRAINEMENT) )")
+
+st.markdown("**R4 : Quelle est la liste des séances d'entrainement à haute intensité ?**")
+st.latex(r"R_4 = \sigma_{duree\_minutes > 90 \land rpe\_session > 8} (ENTRAINEMENT)")
+
+st.markdown("**R5 : Quels athlètes ont pratiqué sur l'intégralité des surfaces de jeu répertoriées ?**")
+st.latex(r"R_5 = \pi_{id\_athlete, surface\_jeu} (ENTRAINEMENT) \div \pi_{surface\_jeu} (ENTRAINEMENT)")
+
+
+st.subheader("Bloc 3 : Monitoring Physiologique (Santé)")
+
+st.markdown("**R6 : Quels sont les athlètes en état d'alerte ?**")
+st.latex(r"R_6 = \pi_{id\_athlete} ( \sigma_{niveau\_stress > 0.8} (INDICATEUR\_PHYSIO) ) \cup \pi_{id\_athlete} ( \sigma_{qualite\_sommeil < 3} (INDICATEUR\_PHYSIO) )")
+
+
+st.subheader("Bloc 4 : Analyse des Incidents (Blessures)")
+
+st.markdown("**R7 : Quelles sont les surfaces de jeu à risque chez les femmes ?**")
+st.latex(r"R_7 = \pi_{surface\_jeu} ( (ATHLETE \bowtie ENTRAINEMENT \bowtie BLESSURE) \bowtie \sigma_{genre = 'Female'} (ATHLETE) )")
+
+st.markdown("**R8 : Quels athlètes ne se sont jamais blessés ?**")
+st.latex(r"R_8 = \pi_{id\_athlete} (ATHLETE) - \pi_{id\_athlete} (ENTRAINEMENT \bowtie BLESSURE)")
+
+st.markdown("**R9 : Pour chaque athlète blessé, quelle est la liste des athlètes du même sport ?**")
+st.latex(r"R_9 = \pi_{A1.id\_athlete, A2.id\_athlete} ( \sigma_{A1.id\_athlete \neq A2.id\_athlete} ( (ATHLETE_{A1} \bowtie \pi_{id\_athlete}(ENTRAINEMENT \bowtie BLESSURE)) \bowtie_{sport} ATHLETE_{A2} ) )")
+
+
+st.subheader("Bloc 5 : Conformité et Protocoles")
+
+st.markdown("**R10 : Quels sont les athlètes ayant réalisé l'intégralité du protocole ?**")
+st.latex(r"R_{10} = \pi_{id\_athlete, id\_test} (PASSER) \div \pi_{id\_test} (TEST\_MEDICAL)")
+
 
 st.markdown("---")
 
